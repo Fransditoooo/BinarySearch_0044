bb39d131c581fcc853cbc65d6d6a6a3108f4343c
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-07 20:08:31 +0700

    Implementasi Algoritma Bubble Sort

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -31,4 +31,22 @@ void input()
     }
 }
 
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int i = 0; i <= nPanjang - pass - 1; i++)
+        {
+            if (element[i] > elemen[i + 1])
+            {
+                int temp = elemen[i];
+                elemen[i] = elemen[i + 1];
+                elemen[i + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
 
